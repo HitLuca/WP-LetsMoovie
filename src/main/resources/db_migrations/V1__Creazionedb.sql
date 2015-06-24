8c4cb8d8614ec879edb574ee7cb1ec8febd96cb3
Etrunon <bosottiluca@gmail.com>
2015-06-24 17:16:11 +0200

    Commit for update and sync with backenders and databaser.

--- src/main/resources/db_migrations/V1__Creazionedb.sql
+++ src/main/resources/db_migrations/V1__Creazionedb.sql
@@ -1,40 +1,40 @@
-CREATE TABLE cinema_rooms(
+CREATE TABLE cinema_rooms (
   room_number INT,
   length INT,
-  width INT,
+  width  INT,
   PRIMARY KEY (room_number)
 );
 
 CREATE TABLE seats (
-  id_seat SERIAL,
+  id_seat  SERIAL,
   room_number INT,
-  row INT,
+  row      INT,
   "column" INT,
-  status TEXT,
+  status   TEXT,
   PRIMARY KEY (id_seat),
-  FOREIGN KEY(room_number) REFERENCES cinema_rooms(room_number)
+  FOREIGN KEY (room_number) REFERENCES cinema_rooms (room_number)
 );
 
 CREATE TABLE films (
-  id_film SERIAL,
+  id_film   SERIAL,
   film_title TEXT,
-  poster TEXT,
-  duration SMALLINT,
-  trailer TEXT,
+  poster    TEXT,
+  duration  SMALLINT,
+  trailer   TEXT,
   metascore SMALLINT,
-  rating DECIMAL(2,1),
-  year SMALLINT,
-  plot TEXT,
-  director TEXT,
-  VM INT,
+  rating    DECIMAL(2, 1),
+  year      SMALLINT,
+  plot      TEXT,
+  director  TEXT,
+  VM        INT,
   PRIMARY KEY (id_film)
 );
 
-CREATE TABLE film_genres(
+CREATE TABLE film_genres (
   id_film SERIAL,
   genre TEXT,
   PRIMARY KEY (id_film),
-  FOREIGN KEY (id_film) REFERENCES films(id_film)
+  FOREIGN KEY (id_film) REFERENCES films (id_film)
 );
 
 CREATE TABLE actors (
@@ -46,55 +46,56 @@ CREATE TABLE actors (
 CREATE TABLE film_actors (
   id_film INT,
   actor_name TEXT,
-  role TEXT,
+  role    TEXT,
   PRIMARY KEY (id_film, actor_name, role),
-  FOREIGN KEY(id_film) REFERENCES films(id_film),
-  FOREIGN KEY(actor_name) REFERENCES actors(actor_name)
+  FOREIGN KEY (id_film) REFERENCES films (id_film),
+  FOREIGN KEY (actor_name) REFERENCES actors (actor_name)
 );
 
 CREATE TABLE users (
-  id_user SERIAL,
-  email TEXT,
-  name TEXT,
-  surname TEXT,
-  password TEXT,
-  phone_number INT,
-  birthday DATE,
+  id_user         SERIAL,
+  email           TEXT,
+  name            TEXT,
+  surname         TEXT,
+  username        TEXT,
+  password        TEXT,
+  phone_number    INT,
+  birthday        DATE,
   credit_card_number INT,
   residual_credit MONEY,
-  role TEXT,
-  PRIMARY KEY(id_user)
+  role            TEXT,
+  PRIMARY KEY (id_user)
 );
 
 CREATE TABLE prices (
   ticket_type TEXT,
   price MONEY,
-  PRIMARY KEY(ticket_type)
+  PRIMARY KEY (ticket_type)
 );
 
 CREATE TABLE shows (
-  id_show SERIAL,
+  id_show   SERIAL,
   room_number INT,
-  id_film INT,
+  id_film   INT,
   show_date DATE,
   show_time TIME,
-  PRIMARY KEY(id_show),
-  FOREIGN KEY(room_number) REFERENCES cinema_rooms(room_number),
-  FOREIGN KEY(id_film) REFERENCES films(id_film)
+  PRIMARY KEY (id_show),
+  FOREIGN KEY (room_number) REFERENCES cinema_rooms (room_number),
+  FOREIGN KEY (id_film) REFERENCES films (id_film)
 );
 
 CREATE TABLE payments (
-  id_user INT,
+  id_user     INT,
   payment_date DATE,
   payment_time TIME,
   ticket_type TEXT,
-  id_seat INT,
-  id_show INT,
+  id_seat     INT,
+  id_show     INT,
   PRIMARY KEY (id_user, payment_date, payment_time, ticket_type, id_seat, id_show),
-  FOREIGN KEY(id_user) REFERENCES users(id_user),
-  FOREIGN KEY(ticket_type) REFERENCES prices(ticket_type),
-  FOREIGN KEY(id_seat) REFERENCES seats(id_seat),
-  FOREIGN KEY(id_show) REFERENCES shows(id_show)
+  FOREIGN KEY (id_user) REFERENCES users (id_user),
+  FOREIGN KEY (ticket_type) REFERENCES prices (ticket_type),
+  FOREIGN KEY (id_seat) REFERENCES seats (id_seat),
+  FOREIGN KEY (id_show) REFERENCES shows (id_show)
 );
 
 CREATE TABLE seat_reservations (
@@ -102,6 +103,6 @@ CREATE TABLE seat_reservations (
   id_seat INT,
   status TEXT,
   PRIMARY KEY (id_show, id_seat),
-  FOREIGN KEY(id_show) REFERENCES shows(id_show),
-  FOREIGN KEY(id_seat) REFERENCES seats(id_seat)
+  FOREIGN KEY (id_show) REFERENCES shows (id_show),
+  FOREIGN KEY (id_seat) REFERENCES seats (id_seat)
 );
