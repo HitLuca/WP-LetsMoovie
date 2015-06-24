6b1d93496499678e13720489ab4b97d38a2a7797
M4rc0SX <federicimarcosx@gmail.com>
2015-06-24 18:26:27 +0200

    Add insert query to userMapper

--- src/main/java/types/mappers/UserMapper.java
+++ src/main/java/types/mappers/UserMapper.java
@@ -1,6 +1,9 @@
 package types.mappers;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+
+import java.sql.Date;
 
 /**
  * Created by marco on 24/06/15.
@@ -8,4 +11,14 @@ import org.apache.ibatis.annotations.Select;
 public interface UserMapper {
     @Select("SELECT password FROM users WHERE username = #{user}")
     String getUserCredential(String user);
+
+    @Insert("INSERT INTO users (username, password, email, birthday, name, surname, phone) SELECT " +
+            "#{username}," +
+            "#{password}," +
+            "#{email}," +
+            "#{birthday}," +
+            "#{name}," +
+            "#{surname}," +
+            "#{phone}")
+    void insertUser(String username,String password, String email, Date birthday, String name,String surname, String phone);
 }
