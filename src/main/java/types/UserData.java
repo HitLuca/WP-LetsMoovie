a40e96211ca7f27622e7ade4c2357805780100fd
HitLuca <luca.simonetto.94@gmail.com>
2015-06-26 13:29:59 +0200

    aggiunto tipo UserData per accomodare i dati della query getUserData del mapper

---
+++ src/main/java/types/UserData.java
@@ -0,0 +1,88 @@
+package types;
+
+/**
+ * Created by hitluca on 26/06/15.
+ */
+public class UserData {
+    String email;
+    String name;
+    String surname;
+    String username;
+    String password;
+    String phone_number;
+    String birthday;
+    float residual_credit;
+    int role;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public float getResidual_credit() {
+        return residual_credit;
+    }
+
+    public void setResidual_credit(float residual_credit) {
+        this.residual_credit = residual_credit;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+}
