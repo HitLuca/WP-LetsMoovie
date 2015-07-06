90adc9805b82569f0f25c6173379bce5af7a59cc
mion00 <mion00@gmail.com>
2015-07-06 11:23:40 +0200

    Rebase per sistemare i merge Aggiunto login status

---
+++ src/main/java/types/LoginStatus.java
@@ -0,0 +1,19 @@
+package types;
+
+/**
+ * Created by marco on 06/07/15.
+ */
+public class LoginStatus {
+
+    private boolean status;
+    private String username;
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
