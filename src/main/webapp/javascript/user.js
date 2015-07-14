3e5fc5f3b61b78d4404e21d2202f450b455ed1f8
mion00 <mion00@gmail.com>
2015-07-14 17:26:52 +0200

    Aggiunto gestore di date JS Refactoring user.js

--- src/main/webapp/javascript/user.js
+++ src/main/webapp/javascript/user.js
@@ -1,6 +1,39 @@
 var User = {
+    username: Session.getUsername(),
     displayUserData: function () {
-
+        $.getJSON("/api/user/" + User.username)
+            .done(function (data) {
+                var directives = {
+                    birthday: {
+                        text: function (params) {
+                            moment.locale("it");
+                            return new moment(this.birthday).format("LL");
+                        }
+                    }
+                };
+                $("#panel1a").render(data, directives);
+            })
+            .fail(function (data) {
+                var error = data.responseJSON.errorCode;
+                switch (error) {
+                    case 0:
+                    case 2:
+                    {
+                        alertify.error("Errore!");
+                    }
+                        break;
+                    case 8:
+                    {
+                        alertify.error("Non sei autorizzato a accedere a questa risorsa!");
+                    }
+                        break;
+                    case 10:
+                    {
+                        Session.redirectToLogin();
+                    }
+                        break;
+                }
+            });
     }
 };
 
@@ -10,13 +43,7 @@ $(function () {
     if (name == null) {
         Session.redirectToLogin();
     } else {
-        $.getJSON("/api/user/"+name)
-            .done(function (data) {
-                $("#panel1a").render(data);
-            })
-            .fail(function (data) {
-
-            });
+        User.displayUserData();
     }
 
 
