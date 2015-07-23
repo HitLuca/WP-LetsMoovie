254f8cfd1a92ee0cfab79c33b200b26d90558ec3
lucaAlberigo <luca.alberigo217@gmail.com>
2015-07-23 19:24:26 +0200

    fatta listino prezzi finito

---
+++ src/main/webapp/javascript/listinoPrezzi.js
@@ -0,0 +1,30 @@
+/**
+ * Created by fuffaknight on 23/07/15.
+ */
+numeral.language('it');
+
+var Ticket = {
+    getTicket: function () {
+        $.ajax({
+            dataType: "json",
+            url: "/api/tickets"
+        }).done(Ticket.displayTicket);
+    },
+    displayTicket: function (data) {
+        console.log(data);
+        var directives = {
+
+            price: {
+                text: function () {
+                    return numeral(this.price).format('0,0.00 $');
+                }
+            }
+        };
+        Transparency.render($("#allTickets")[0], data.tickets, directives);
+    }
+}
+
+
+$(function () {
+    Ticket.getTicket();
+});
